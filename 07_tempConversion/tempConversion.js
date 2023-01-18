7dedea61c9082fd2e617a0482db798f93c8c43e5
Chris <chris.a.proc@gmail.com>
2023-01-18 16:15:39 -0700

    Exercise 07 Complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreesF) {
+  let degreesC = (degreesF - 32) * 5/9;
+  return roundToOne(degreesC);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreesC) {
+  let degreesF = ((degreesC * 9/5)+32);
+  return roundToOne(degreesF);
 };
+
+function roundToOne(number){
+  return +(Math.round(number+"e+1") + "e-1");
+}
+
+
 
 // Do not edit below this line
 module.exports = {
