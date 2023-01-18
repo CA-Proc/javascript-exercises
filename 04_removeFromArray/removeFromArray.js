331830e5614b82ab7771d9dde3a75fa1bc5139ef
Chris <chris.a.proc@gmail.com>
2023-01-17 17:20:37 -0700

    Exercise set 04 complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, ...removeItems) {
 
+    for(let i = 0; i<(removeItems.length); i++){
+
+        itemIndex = inputArray.indexOf(removeItems[i]);
+
+        if (itemIndex > -1){
+            inputArray.splice(itemIndex,1);
+        }
+    }
+
+    return inputArray;
 };
 
 // Do not edit below this line
