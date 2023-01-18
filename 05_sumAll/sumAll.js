37e42b00fd758f2c21c62cb2b78d0674f1e131ee
Chris <chris.a.proc@gmail.com>
2023-01-17 17:33:10 -0700

    Exercise 05 Completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
 
+    if (!isValid(a) || !isValid(b)){
+        return 'ERROR'
+    }
+
+    let sum = 0;
+    for(let i = Math.min(a,b); i<=Math.max(a,b); i++){
+        sum += i;
+    }
+
+    return sum;
 };
 
+function isValid(number){
+    
+    if (typeof number === 'number' && ((number%1) == 0) && (number >= 0)) {
+        return true;
+    }
+
+    return false;
+}
 // Do not edit below this line
 module.exports = sumAll;
